75d39d02a22524f2cc65fcba799c804e5f4f5d7a
leoopl <leo.leite@hotmail.com>
2023-02-27 15:14:56 -0300

    new chat

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,34 @@ type MessageLog = {
 	message: string;
 };
 
+type Chats = {
+	id: number;
+	name: string;
+	msg: MessageLog[];
+};
+
 export default function App() {
 	const [input, setInput] = useState<string>('');
 	const [messageLog, setMessageLog] = useState<MessageLog[]>([]);
+
+	const [chatGroup, SetChatGroup] = useState<Chats[]>([]);
+
+	// ADD A NEW MESSAGE TO SPECIFIC CHAT
+	// const updateChatGroup = (chatGroup: Chats[], chatId: number, newMessage: MessageLog) => {
+	// 	return chatGroup.map(chat => {
+	// 		if (chat.id !== chatId) return chat;
+	// 		return { ...chat, msg: [...chat.msg, newMessage] };
+	// 	});
+	// };
+
+	// const handleNewMessage = () => {
+	// 	let newMsg = { user: 'me', message: input };
+	// 	SetChatGroup(prevChatGroup => updateChatGroup(prevChatGroup, 2, newMsg));
+	// 	const answerMsg = { user: 'ChatGPT', message: 'How can I help you today?' };
+	// 	SetChatGroup(prevChatGroup => updateChatGroup(prevChatGroup, 2, answerMsg));
+	// 	setInput('');
+	// 	console.log(chatGroup);
+	// };
 
 	const clearLog = () => {
 		setMessageLog([]);
@@ -25,10 +50,32 @@ export default function App() {
 		setMessageLog(newLog);
 	};
 
+	const handleNewChat = () => {
+		const newIndex = chatGroup.length + 1;
+		let newChat = [
+			...chatGroup,
+			{
+				id: newIndex,
+				name: `Chat ${newIndex}`,
+				msg: [],
+			},
+		];
+		SetChatGroup(newChat);
+	};
+
 	return (
 		<div className={style.container}>
 			<section className={style.sideBar}>
-				<div className={style.sideBarTopButton}>New Chat</div>
+				<div onClick={handleNewChat} className={style.sideBarTopButton}>
+					New Chat
+				</div>
+				<div className={style.chatGroup}>
+					{chatGroup.map(chat => (
+						<div key={chat.id} className={style.sideBarChatButton}>
+							{chat.name}
+						</div>
+					))}
+				</div>
 				<div>
 					<hr className={style.divider} />
 					<div onClick={clearLog} className={style.sideBarBottomButton}>
@@ -49,6 +96,7 @@ export default function App() {
 				</div>
 				<div className={style.inputBox}>
 					<input
+						disabled={chatGroup.length === -1}
 						onKeyDown={e => e.key === 'Enter' && input != '' && handleInput()}
 						type="text"
 						value={input}
